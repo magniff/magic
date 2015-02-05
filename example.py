64f8ed39f257459bc842de5a59c0dd718ff2a276
Alexander Koshkin <akoshkin@ptsecurity.ru>
2015-02-05 19:27:01 +0300

    Add singletone example.

--- example.py
+++ example.py
@@ -2,7 +2,17 @@ from override import Builder, context
 
 
 def my_callback(name, bases, attrs):
-    pass
+
+    def __new__(cls, *args, **kwargs):
+        _, parent, *_ = cls.__mro__
+
+        if cls.instance is None:
+            cls.instance = parent.__new__(parent, *args, **kwargs)
+
+        return cls.instance
+
+    attrs['instance'] = None
+    attrs['__new__'] = __new__
 
 
 Builder.register_metaclass_callback(my_callback)
@@ -14,3 +24,11 @@ with context(Builder):
 
     class B(A):
         pass
+
+    class C(B, A):
+        pass
+
+
+assert C() is A()
+assert A() is A()
+assert A() is B()
